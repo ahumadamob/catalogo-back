4c34fbf7ff4756dba4cb61336433c8223383f3ac
Mario Ahumada <ahumadamob@gmail.com>
2025-06-29 19:57:08 -0300

    agrega DTO ApiResponse y clases auxiliares

---
+++ src/main/java/com/example/catalogo/dto/Pagination.java
@@ -0,0 +1,17 @@
+package com.example.catalogo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pagination {
+    private int page;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+}
